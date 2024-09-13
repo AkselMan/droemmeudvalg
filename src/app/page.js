14aae3588907f832f2fa0a22ba2b4b9b97dbc015
Aksel Holbech <aksel.holbech@gmail.com>
2024-09-13 23:54:06 +0200

    changed text

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="bg-green-800 w-full my-16 lg:my-0 lg:w-1/4 p-8 rounded-xl text-2xl lora text-white leading-relaxed">
         <h1 className="text-4xl text-center items-center justify-center mb-8 calistoga">Hvad er Drømmeudvalget?</h1>
         <p><b>Drømmeudvalget</b> er nok det fedeste elev-udvalg på <a className="text-yellow-400 underline italic  " href="https://cg-gym.dk">Christianshavns Gymnasium</a>!</p>
-        <p>Drømmeudvalgets mål er at spræde liv og glæde på CG igennem deres mange aktiviter udvalget står for!</p>
+        <p>Drømmeudvalgets mål er at spræde liv og glæde på CG igennem de mange aktiviter udvalget står for!</p>
         <p>Du kan læse mere om Drømmeudvalgets aktiviter nedenfor.</p>
         
         <div className="flex w-12 mx-auto mt-12">
@@ -22,8 +22,8 @@ export default function Home() {
       
       <div className="bg-rose-600 my-12 w-full lg:w-1/4 p-5 rounded-xl text-2xl">
         <h1 className="text-4xl text-center items-center justify-center mb-8 font-bold text-black cali">Aktiviteter</h1>
-          <Aktivitet title="Søjlecafée!" img="/Cafe/2xstorm.png"desc={<p>I løbet af året afholder Drømmeudvalget <b>Søjlecaféer</b>, som er caféer, der afholdes særligt for hver studieretning.</p>}/>
-          <Aktivitet title="Santa Lucia" img="/SantaLucia/SantaLucia.png" desc={<p>Engleklang og smurte kroppe, hvad mere kunne man ønske sig til jul? Drømmeudvalget står hvert December måned for at levere det fedeste <b>Luciaoptog</b>.</p>} />
+          <Aktivitet title="Søjlecafée!" img="/Cafe/2xstorm.png"desc={<p>I løbet af året afholder Drømmeudvalget <b>Søjlecaféer</b>, som er caféer, der arrangeres særligt for hver studieretning.</p>}/>
+          <Aktivitet title="Santa Lucia" img="/SantaLucia/SantaLucia.png" desc={<p>Engleklang og smurte kroppe, hvad mere kunne man ønske sig til jul? Hver December måned Drømmeudvalget for at levere det fedeste <b>Luciaoptog</b>.</p>} />
           <Aktivitet title="Valentinshilsner" img="/Valentin/2g.png" desc={<p>Drømmeudvalget sørger for kærligheden! For kun <span className="underline font-bold italic">5 kr.</span> kan du frem til Valentinsdag sende en kærlighedsfe ud med en personlig hilsen og en slikkepind til dit crush eller en ven.</p>} />
       </div>
 
