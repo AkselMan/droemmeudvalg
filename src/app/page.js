c430ee7ba505ad0b34306d7586f49b71d1cc1754
Aksel Holbech <aksel.holbech@gmail.com>
2024-09-13 23:01:02 +0200

    added png istedet for jpg, og ig follow card

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Aktivitet from "./components/Aktivitet";
 import Link from "next/link";
 
+
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center p-5">
@@ -21,9 +22,15 @@ export default function Home() {
       
       <div className="bg-rose-600 my-12 w-full lg:w-1/4 p-5 rounded-xl text-2xl">
         <h1 className="text-4xl text-center items-center justify-center mb-8 font-bold text-black cali">Aktiviteter</h1>
-          <Aktivitet title="Søjlecafée!" img="/cafe/Lys.jpg"desc={<p>I løbet af året afholder Drømmeudvalget <b>Søjlecaféer</b>, som er caféer, der afholdes særligt for hver studieretning.</p>}/>
+          <Aktivitet title="Søjlecafée!" img="/cafe/2xstorm.png"desc={<p>I løbet af året afholder Drømmeudvalget <b>Søjlecaféer</b>, som er caféer, der afholdes særligt for hver studieretning.</p>}/>
           <Aktivitet title="Santa Lucia" img="/SantaLucia/SantaLucia.png" desc={<p>Engleklang og smurte kroppe, hvad mere kunne man ønske sig til jul? Drømmeudvalget står hvert December måned for at levere det fedeste <b>Luciaoptog</b>.</p>} />
           <Aktivitet title="Valentinshilsner" img="/Valentin/2g.png" desc={<p>Drømmeudvalget sørger for kærligheden! For kun <span className="underline font-bold italic">5 kr.</span> kan du frem til Valentinsdag sende en kærlighedsfe ud med en personlig hilsen og en slikkepind til dit crush eller en ven.</p>} />
+      </div>
+
+      <div className="bg-sky-800 text-sm text-white w-full lg:w-1/4 p-5 rounded-xl text-2xl flex flex-col items-center space-between">
+      <h1 className="text-2xl  mb-5">Følg Drømmeudvalget på IG</h1>
+        <Link href="https://www.instagram.com/droemmeudvalget.cg/"><Image className="rounded-full mb-2" alt="logo" src={"/ig.png"} width={200} height={200} /></Link>
+        <p className="text-lg">Der meget mere om Drømmeudvalget på Instagram</p>
       </div>
 
       <div className="bg-slate-600 my-12 text-sm w-full lg:w-1/4 p-5 rounded-xl text-2xl flex flex-row space-between">
